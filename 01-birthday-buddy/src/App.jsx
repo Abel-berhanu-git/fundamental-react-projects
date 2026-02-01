9dabadf5d3bef8532212c90913ee7a7e684ecf7e
AbelBerhanu <abelberhanu2018@gmail.com>
2026-02-01 11:55:12 +0300

    adding finished 01-birthday-buddy

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import List from './List';
+import userList from './data';
 
 const App = () => {
-  return <List />;
+  const [users, setUsers] = useState(userList);
+  return (
+    <main>
+      <section className='container'>
+        <h3> {users.length} birthdays today</h3>
+        <List users={userList} />
+        <div
+          className='btn clear-all'
+          onClick={() => {
+            setUsers([]);
+          }}
+        >
+          clear all
+        </div>
+      </section>
+    </main>
+  );
 };
 export default App;
--- 01-birthday-buddy/src/List.jsx
+++ 01-birthday-buddy/src/List.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react';
 import Person from './Person';
-import userList from './data';
 
-const List = () => {
- const [users, setUsers] = useState(userList);
-
+const List = ({ users }) => {
   return (
-    <div className='container'>
-      <h3> {users.length} birthdays today</h3>
+    <div>
       {users.map((user) => {
         return <Person key={user.id} {...user} />;
       })}
-      <div className="btn clear-all" onClick={()=>{setUsers([])}}>clear all</div>
     </div>
   );
 };
